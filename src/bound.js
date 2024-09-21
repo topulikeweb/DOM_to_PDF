a98d046e40eba82e955e8c7e458b531142c3e12e
郑朝洋 <2831656901@qq.com>
2024-09-21 10:57:17 +0800

    修复翻页的问题

--- src/bound.js
+++ src/bound.js
@@ -5,46 +5,61 @@ class PDFCreator {
   pageSize
   pdf
   
-  constructor (targetContent, pageSize) {
-    this.pageSize = pageSize
+  constructor (targetContent) {
+    this.pageSize = 'a4'
     this.pdf = new jsPDF({
       orientation: "portrait",
-      unit: "mm",
+      unit: "px",
       format: this.pageSize,
     })
     this.targetContent = targetContent
   }
   
-  /**
-   * 转换单位px->mm
-   */
-  
-  convertUnit (num) {
-    num = (num / 3.78).toFixed(3)
-    return Number(num)
-  }
-  
   exportPDF () {
-    html2canvas(this.targetContent).then(canvas => {
-      const imageData = canvas.toDataURL('image/png', 0.5);
-      // 获取图片宽高比来适应PDF页面
-      const imgWidth = this.convertUnit(this.targetContent.offsetWidth);
-      const imgHeight = this.convertUnit((canvas.height * this.targetContent.offsetWidth) / canvas.width);
-      const pageHeight = this.pdf.internal.pageSize.getHeight()
-      let remainHeight = imgHeight
-      let position = 0
-      while (remainHeight > 0) {
-        const currentHeight = Math.min(imgHeight, remainHeight)
-        this.pdf.addImage(imageData, 'JPG', 0, -position, imgWidth, imgHeight)
-        remainHeight -= currentHeight
-        position += currentHeight
-        if (currentHeight > 0) {
-          this.pdf.addPage()
+    html2canvas(this.targetContent, {
+      allowTaint: false,
+      useCORS: true,
+      scale: 2
+    }).then((canvas) => {
+      let contentWidth = canvas.width;
+      let contentHeight = canvas.height;
+      //一页pdf显示html页面生成的canvas高度;
+      let pageHeight = contentWidth / 592.28 * 841.89;
+      //未生成pdf的html页面高度
+      let leftHeight = contentHeight;
+      //页面偏移
+      let position = 0;
+      //a4纸的尺寸[595.28,841.89]
+      let imgWidth = 595.28;
+      let imgHeight = 592.28 / contentWidth * contentHeight;
+      let pageData = canvas.toDataURL('image/jpeg', 1.0);
+      //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+      //当内容未超过pdf一页显示的范围，无需分页
+      if (leftHeight < pageHeight) {
+        this.pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight);
+      } else {    // 分页
+        while (leftHeight > 0) {
+          this.pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+          leftHeight -= pageHeight;
+          position -= 841.89;
+          //避免添加空白页
+          if (leftHeight > 0) {
+            this.pdf.addPage();
+          }
         }
       }
-      this.pdf.save("download.pdf");
-    });
+      this.pdf.save('download.pdf');
+    })
   }
 }
+
+const targetContent = document.getElementById('content')
+
+
+const downloadBtn = document.getElementById('download')
+downloadBtn.addEventListener('click', () => {
+  const pdfCreator = new PDFCreator(targetContent);
+  pdfCreator.exportPDF();
+})
 
 
